bf090f0d7acecb2e7870a4d8043bd88ff681b1fd
Fafalilove12github <122897675+Fafalilove12github@users.noreply.github.com>
2026-01-09 07:33:26 +0000

    Import Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import React from 'react';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 function App() {
   const [count, setCount] = useState(0)
 
